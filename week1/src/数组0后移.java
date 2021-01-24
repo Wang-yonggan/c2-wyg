4e458643559c07d3a9b1233f838e4edb9a5a8874
Your Name <you@example.com>
2021-01-24 14:39:36 +0800

    push you comment here please!

--- week1/src/数组0后移.java
+++ week1/src/数组0后移.java
@@ -1,9 +1,12 @@
 import java.util.Scanner;
 //原地置换
-public class 数组0后移移 {
+public class 数组0后移 {
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
-        int[] nums={1,2,0,3,0,4,0,5};
+        int[] nums=new int[5];
+        for(int i=0;i<5;i++){
+            nums[i]=in.nextInt();
+        }
         int last=0,first=0,temp,cnt=0;
         while(last<nums.length){
             if(nums[last]!=0){       //如果last指向的不为0，与第一个0（即first）交换
@@ -21,6 +24,6 @@ public class 数组0后移移 {
             last++;
         }
         for(int i=0;i<nums.length;i++)
-            System.out.println(nums[i]);
+            System.out.print(nums[i]+" ");
     }
 }
