1959a749b2fd56f2f07e45ddcfbaeebf580ee3b7
Your Name <you@example.com>
2021-02-01 19:13:30 +0800

    Merge remote-tracking branch 'origin/master' into master

--- week2/src/interface_/Star.java
+++ week2/src/interface_/Star.java
@@ -4,6 +4,10 @@ public class Star implements Universe {
 
     @Override
     public void doAnything() {
-
+        System.out.println("star:接口函数的实例化");
     }
+    public void shine(){
+        System.out.println("star:星星一闪一闪亮晶晶");
+    }
+
 }
