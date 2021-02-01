fe44447f1dc5373a99ec1c4fc33d5e70196f3941
Your Name <you@example.com>
2021-02-01 20:17:57 +0800

    Merge remote-tracking branch 'origin/master' into master

--- week2/src/abc/A.java
+++ week2/src/abc/A.java
@@ -3,9 +3,6 @@ package abc;
 public abstract class A {
     private int numa = 10;
 
-    public void setNuma(int numa) {
-        this.numa = numa;
-    }
 
     public int getNuma() {
         return numa;
