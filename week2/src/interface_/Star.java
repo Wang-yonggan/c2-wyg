4230c82df61198cc9f0964cf9f20512f7dca4a58
Your Name <you@example.com>
2021-02-04 10:56:33 +0800

    改正sun的错误继承
    Signed-off-by: Your Name <you@example.com>

--- week2/src/interface_/Star.java
+++ week2/src/interface_/Star.java
@@ -1,11 +1,6 @@
 package interface_;
 
-public class Star implements Universe {
-
-    @Override
-    public void doAnything() {
-        System.out.println("star:接口函数的实例化");
-    }
+public class Star {
 
     public void shine() {
         System.out.println("star:星星一闪一闪亮晶晶");
--- week2/src/interface_/Sun.java
+++ week2/src/interface_/Sun.java
@@ -1,8 +1,13 @@
 package interface_;
 
-public class Sun extends Star {
+public class Sun extends Star implements Universe {
     @Override
     public void shine() {
         System.out.println("sun:光照8分钟，到达地球");
+    }
+
+    @Override
+    public void doAnything() {
+        System.out.println("sun:太阳吸引九大行星旋转");
     }
 }
