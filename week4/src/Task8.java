71e24c239154a37a54d55aaf7b11c2fd0b9b54b2
wangyonggan <2245316014@qq.com>
2021-02-24 19:57:22 +0800

    文件操作
    Signed-off-by: wangyonggan <2245316014@qq.com>

--- week4/src/Task8.java
+++ week4/src/Task8.java
@@ -1,3 +1,11 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * @ClassName : Task8
  * @Author : wang-yonggan
@@ -5,4 +13,30 @@
  */
 
 public class Task8 {
+    public static void main(String[] args) {
+        //写入内容
+        BufferedWriter bf;
+        try (FileWriter fw = new FileWriter("C:\\csatest\\demo.txt")) {
+            bf = new BufferedWriter(fw);
+            bf.write("csa2021csa2021csa2021csa2021csa2021csa2021csa2021csa2021csa2021csa2021csa2021csa20" +
+                    "21csa2021csa2021csa2021csa2021csa2021csa2021csa2021csa2021csa2021csa2021csa2021csa2" +
+                    "021csa2021csa2021csa2021csa2021csa2021csa2021csa2021csa2021csa2021csa2021csa2021");
+            bf.close();
+            System.out.println("写入成功");
+            //查询有多少个2021
+            List<String> all_line = new LinkedList<>();
+            String all_content = "";
+            all_line = Files.readAllLines(Paths.get("C:\\csatest\\demo.txt"));
+            for (String str : all_line)
+                all_content += str;
+            int len = all_content.split("2021").length;
+            if (!all_content.endsWith("2021")) len--;
+            System.out.printf("共有%d个2021", len);
+        } catch (IOException e) {
+            System.out.println("文件读写错误");
+            e.printStackTrace();
+        }
+
+
+    }
 }
---
+++ week4/src/Task9.java
@@ -0,0 +1,29 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+/**
+ * @ClassName : Task9
+ * @Author : wang-yonggan
+ * @Date: 2021-02-24 15:40
+ */
+
+public class Task9 {
+    public static void main(String[] args) {
+        try (FileWriter fw = new FileWriter("C:\\csatest\\demo2.txt");) {
+
+            List<String> all_line = Files.readAllLines(Paths.get("C:\\csatest\\demo.txt"));
+            for (String line : all_line)
+                fw.write(line);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println();
+        System.out.println("写入成功");
+
+
+    }
+}
